4ae101216910259b7bede2ecf83c15824423ae13
MarieCHessler <marie@hessler.se>
2022-03-18 07:12:45 +0000

    Add hover function to buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-$("p").click(function() {
+/*$("p").click(function() {
     //$("p").slideToggle("slow"); // This hides both paragraphs at the same time
     $(this).slideToggle("slow"); // Hides just the clicked paragaph
-})
+})*/
+
+$("button").mouseenter(function() {
+    $(this).removeClass("makeRed").addClass("makeBlue");
+});
+
+$("button").mouseleave(function() {
+    $(this).removeClass("makeBlue").addClass("makeRed");
+});
