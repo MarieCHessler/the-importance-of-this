544f3abfddf165487dc1b21373c31e3cfd41a006
MarieCHessler <marie@hessler.se>
2022-03-18 07:51:48 +0000

    Add boxes that change color on click

--- js/script.js
+++ js/script.js
@@ -3,10 +3,17 @@
     $(this).slideToggle("slow"); // Hides just the clicked paragaph
 })*/
 
-$("button").mouseenter(function() {
+/*$("button").mouseenter(function() {
     $(this).removeClass("makeRed").addClass("makeBlue");
 });
 
 $("button").mouseleave(function() {
     $(this).removeClass("makeBlue").addClass("makeRed");
+});*/
+
+$(document).ready(function() {
+    $(".box").on("click", function() {
+        var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    });
 });
